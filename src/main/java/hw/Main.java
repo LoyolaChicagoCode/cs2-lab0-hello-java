1b6923f28d7495cd142423a82797bf5f89ddab5e
Konstantin Laufer <laufer@cs.luc.edu>
2013-08-28 13:39:28 -0500

    Added main class and updated build.gradle.

---
+++ src/main/java/hw/Main.java
@@ -0,0 +1,10 @@
+package hw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final HelloWorld instance = new HelloWorld();
+		System.out.println(instance.getMessage());
+		System.out.println(instance.getYear());
+	}
+}
