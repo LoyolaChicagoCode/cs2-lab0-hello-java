aa2195cc8da3707cf67a5cd937a38a10a9726421
Konstantin Läufer <laufer@cs.luc.edu>
2017-09-06 16:51:05 +0000

    minor cosmetic changes
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		System.out.println("args = " + Arrays.asList(args));
 		final HelloWorld instance = new HelloWorld();
 		System.out.println(instance.getMessage());
 		System.out.println(instance.getYear());
+		System.out.println("bye for now");
 	}
 }
